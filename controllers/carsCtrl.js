148019e16a6bf376d7978c623f241d175ddea993
bkchu <brandon.k.chung@gmail.com>
2018-02-28 13:05:29 -0600

    finished &&

--- controllers/carsCtrl.js
+++ controllers/carsCtrl.js
@@ -15,9 +15,69 @@ const cars = [
     model: "DeLorean"
   }
 ];
-
+let id = 4;
 module.exports = {
   getCars: (req, res, next) => {
+    res.status(200).json(cars);
+  },
+
+  addCar: (req, res, next) => {
+    if (req.body) {
+      let newCar = {
+        id: id,
+        make: req.body.make,
+        model: req.body.model
+      };
+      id++;
+      cars.push(newCar);
+      res.status(200).json(cars);
+    } else {
+      res.status(500).json({ message: "Invalid Car" });
+    }
+  },
+
+  updateCar: (req, res, next) => {
+    const selected = cars.filter(val => val.id === parseInt(req.params.id))[0];
+    if (!selected) {
+      res.status(500).json({ message: "Car Not Found" });
+    } else {
+      selected.make = req.body.make;
+      res.status(200).json(cars);
+    }
+  },
+
+  resetCars: (req, res, next) => {
+    const carsReset = [
+      {
+        id: 1,
+        make: "Pontiac",
+        model: "Fiero"
+      },
+      {
+        id: 2,
+        make: "BMW",
+        model: "320"
+      },
+      {
+        id: 3,
+        make: "DMC",
+        model: "DeLorean"
+      }
+    ];
+    cars.splice(0, cars.length);
+    for (let car of carsReset) {
+      cars.push(car);
+    }
+    id = 4;
+    res.status(200).json(cars);
+  },
+
+  deleteCar: (req, res, next) => {
+    const selected = cars.findIndex(val => val.id === parseInt(req.params.id));
+    cars.splice(selected, 1);
+    cars.forEach((car, index, arr) => {
+      car.id = index + 1;
+    });
     res.status(200).json(cars);
   }
 };
--- index.js
+++ index.js
@@ -7,15 +7,21 @@ const carsCtrl = require(`${__dirname}/controllers/carsCtrl`);
 const port = 3001;
 
 const app = express();
-
-app.use((req, res, next) => {
-  console.log("HIT ON EACH REQUEST");
-  next();
-});
 app.use(json());
 app.use(cors());
 
+app.use((req, res, next) => {
+  console.log("HIT ON EACH REQUEST");
+  console.log("REQ.BODY: " + JSON.stringify(req.body));
+  console.log("REQ.PARAMS: " + req.params);
+  next();
+});
+
 app.get("/api/cars", carsCtrl.getCars);
+app.post("/api/cars", carsCtrl.addCar);
+app.put("/api/cars/reset", carsCtrl.resetCars);
+app.put("/api/cars/:id", carsCtrl.updateCar);
+app.delete("/api/cars/:id", carsCtrl.deleteCar);
 
 app.listen(port, () => {
   console.log(`Listening on PORT: ${port}`);
